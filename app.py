439c35c6bd7df4ecf8a09bd79cbf07a87f652ca7
tonghs <tonghuashuai@gmail.com>
2021-01-26 00:10:15 +0800

    nginx image

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    print("hi")
     return f'Hello World!{os.getpid()}'
 
 
