946e3e6d8ffc00ba4cd79716ed61320d16caac70
tonghs <tonghuashuai@gmail.com>
2021-02-07 14:43:57 +0800

    fix

--- ci/restart.sh
+++ ci/restart.sh
@@ -1,4 +1,5 @@
-for i in {1..4}
+END=4
+for i in $(seq 1 $END)
 do
     echo retart web-template_web_$i...
     docker stop web-template_web_$i && docker exec -it web-template_nginx_1 nginx -s reload
