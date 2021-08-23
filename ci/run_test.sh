726c95190a7016b8fd9ed1afcbe8b23fe8005c0e
tonghs <tonghuashuai@gmail.com>
2021-08-23 18:59:11 +0800

    fix test case

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -2,7 +2,7 @@
 
 # prepare
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWD -e "DROP DATABASE IF EXISTS $MYSQL_DB;"
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWD -e "CREATE DATABASE $MYSQL_DB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;"
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWD -e "CREATE DATABASE $MYSQL_DB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWD $MYSQL_DB < ci/tables.sql
 
 py.test --cache-clear -vv -s --cov=models --cov=utils --cov-report=html tests/
