730b9d1f99be62d9a4effc5710e43711d2120f64
tonghs <tonghuashuai@gmail.com>
2021-08-25 22:49:47 +0800

    update task

--- libs/notion.py
+++ libs/notion.py
@@ -9,8 +9,8 @@ class NotionClient:
         self.database_id = database_id
         self.client = Client(auth=token)
 
-    def create_task(self, name: str, assign: List[str], priority: str, status: str, tower_id: int, tower_url: str):
-        data = {
+    def _gen_data(self, name: str, assign: List[str], priority: str, status: str, tower_id: int, tower_url: str):
+        return {
             "parent": {
                 "database_id": self.database_id
             },
@@ -47,13 +47,43 @@ class NotionClient:
                 }
             }
         }
+
+    def create_task(self, name: str, assign: List[str], priority: str, status: str, tower_id: int, tower_url: str):
+        data = self._gen_data(
+            name=name, assign=assign,
+            priority=priority, status=status,
+            tower_id=tower_id, tower_url=tower_url
+        )
+
         self.client.pages.create(**data)
 
-    def get_by_tower_id(self, tower_id: int):
-        pass
+    def update_task(self, page_id: str, name: str, assign: List[str], priority: str, status: str, tower_id: int, tower_url: str):
+        if not page_id:
+            return
 
-    def sync_flag(self) -> bool:
-        return False
+        data = self._gen_data(
+            name=name, assign=assign,
+            priority=priority, status=status,
+            tower_id=tower_id, tower_url=tower_url
+        )
+        self.client.pages.update(page_id=page_id, **data)
+
+    def get_by_tower_id(self, tower_id: int):
+        data = {
+            "filter": {
+                "property": "Tower ID",
+                "number": {
+                    "equals": tower_id
+                }
+            },
+            "sorts": [
+                {
+                    "property": "Tower ID",
+                    "direction": "ascending"
+                }
+            ]
+        }
+        return self.client.databases.query(database_id=self.database_id, **data)
 
     def update(self, properties: Dict):
         pass
