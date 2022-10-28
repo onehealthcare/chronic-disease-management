f5f91e50514fea78ca8d3c74c3cdb13c8a6a5542
仝华帅 <tonghuashuai@gmail.com>
2022-10-28 14:41:20 +0800

    分页 limit bugfix

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def before_request():
             return error(ApiError.invalid_cursor)
 
         try:
-            size = int(request.args.get('size', request.get('limit', 15)))
+            size = int(request.args.get('size', request.args.get('limit', 15)))
         except (ValueError, TypeError):
             pass
 
