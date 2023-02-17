44dd99a4a4af4f17b43aaf955797ec3504377370
tonghs <tonghuashuai@gmail.com>
2023-02-17 18:27:25 +0800

    fix

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -5,4 +5,4 @@ mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWD -e "DROP DATABASE IF EXISTS 
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWD -e "CREATE DATABASE $MYSQL_DB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWD $MYSQL_DB < ci/tables.sql
 
-py.test --cache-clear -vv -s --cov=models --cov=utils --cov-report=xml tests/
+py.test --cache-clear -vv -s --cov=models --cov=utils --cov-report=html tests/
