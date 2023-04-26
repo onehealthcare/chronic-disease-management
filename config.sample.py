642edac810ad15ab66e2a804ad5c3a973139bceb
tonghs <tonghuashuai@gmail.com>
2023-04-26 18:00:03 +0800

    微信小程序登录注册短信 api

--- config.sample.py
+++ config.sample.py
@@ -45,3 +45,8 @@ QCLOUD_CDM_COS_URL_PATTERN = 'https://{}/{}{}'.format(
 # redis
 REDIS_DB = os.environ.get('REDIS_DB', 'test-redis')
 REDIS_URL = f'redis://{REDIS_DB}/0'
+
+# qcloud SMS
+SMS_SDK_APP_ID = "",
+SMS_SIGN_NAME = "",
+SMS_WXAPP_LOGIN_TEMPLATE_ID = ""
--- models/init_db.py
+++ models/init_db.py
@@ -10,26 +10,43 @@ from config import (
     QCLOUD_SECRET_ID,
     QCLOUD_SECRET_KEY,
     REDIS_URL,
+    SMS_SDK_APP_ID,
+    SMS_SIGN_NAME,
+    SMS_WXAPP_LOGIN_TEMPLATE_ID,
     WXAPP_ID,
     WXAPP_SECRET,
 )
 from libs.qcloud import QCloudCOSClient
+from libs.sms import SMSClient
 from playhouse.pool import PooledMySQLDatabase
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatWxa
 
 
-db = PooledMySQLDatabase(MYSQL_DB, user=MYSQL_USER, password=MYSQL_PASSWD,
-                         host=MYSQL_HOST, port=MYSQL_PORT)
+db = PooledMySQLDatabase(
+    MYSQL_DB, user=MYSQL_USER, password=MYSQL_PASSWD,
+    host=MYSQL_HOST, port=MYSQL_PORT
+)
 
 wxapp_client = WeChatWxa(WeChatClient(WXAPP_ID, WXAPP_SECRET))
 
 # 存储用户单据
-qcloud_cos_client = QCloudCOSClient(secret_id=QCLOUD_SECRET_ID,
-                                    secret_key=QCLOUD_SECRET_KEY,
-                                    bucket=QCLOUD_CDM_COS_BUCKET,
-                                    region=QCLOUD_CDM_COS_REGION,
-                                    allow_prefix='*')
+qcloud_cos_client = QCloudCOSClient(
+    secret_id=QCLOUD_SECRET_ID,
+    secret_key=QCLOUD_SECRET_KEY,
+    bucket=QCLOUD_CDM_COS_BUCKET,
+    region=QCLOUD_CDM_COS_REGION,
+    allow_prefix='*'
+)
 
 # redis
 redis_conn = redis.Redis.from_url(REDIS_URL, decode_responses=True)  # 存binary 数据，不需要 decode response
+
+# 微信小程序注册登录发送短信
+wxapp_sms_login_client = SMSClient(
+    secret_id=QCLOUD_SECRET_ID,
+    secret_key=QCLOUD_SECRET_KEY,
+    sdk_app_id=SMS_SDK_APP_ID,
+    sign_name=SMS_SIGN_NAME,
+    template_id=SMS_WXAPP_LOGIN_TEMPLATE_ID
+)
